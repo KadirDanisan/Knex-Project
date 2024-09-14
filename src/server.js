4c7895e39f47511ced4cc2e2256204ba5e9e1411
Kadir DANIŞAN <kadirdansan06@gmail.com>
2024-09-14 14:49:53 +0300

    postman

--- src/server.js
+++ src/server.js
@@ -2,12 +2,13 @@ const express = require("express");
 require("dotenv").config();
 const categoryRoutes = require('./routers/category');
 const postRoutes = require('./routers/post');
+const commentRoutes = require('./routers/comment');
 const app = express();
 
 app.use(express.json());
 app.use('/categories', categoryRoutes);
 app.use('/posts', postRoutes);
-
+app.use('/comments', commentRoutes);
 
 
 app.listen( process.env.PORT || 3000, ()=> {
